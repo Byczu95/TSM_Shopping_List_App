ad7f50cd3788cad3855b362e0e82d213525ec1bc
Szymon <szymon_kaszuba97@wp.pl>
2017-04-11 20:33:12 +0200

    Jest kilka przejsc, ale cos nie tak z przekazywaniem nazwy listy do addProduct

--- app/src/main/java/com/example/szymon/shopping_list_app/MainActivity.java
+++ app/src/main/java/com/example/szymon/shopping_list_app/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.szymon.shopping_list_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_list);
+        setContentView(R.layout.activity_list_of_lists);
+
+        }
+    //https:developer.android.com/training/basics/firstapp/starting-activity.html
+
+    public void onClickDoNewList(View v)
+    {
+        Intent intent = new Intent(this, AddNewListActivity.class);
+        startActivity(intent);
     }
+
+    public void onClickQuit(View v)
+    {
+        System.exit(0);
+    }
+
 }
+
+
+
